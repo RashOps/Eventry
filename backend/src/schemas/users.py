47b87db71dd772d956d94d1051ec59e725a9a047
Rayhan Seh Fred Touboui <touboui.sehfredrayhan@gmail.com>
2026-05-26 21:37:15 +0200

    fix(schemas): fix missing Field import in users schema

--- backend/src/schemas/users.py
+++ backend/src/schemas/users.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, HttpUrl
+from pydantic import BaseModel, EmailStr, HttpUrl, Field
 from typing import Optional
 from datetime import datetime
 from enum import Enum
